c2f1a17f26f52e3f75ccc73462d9124d0cf8104b
glamvian <sidorejokarangploso@gmail.com>
2018-03-30 09:49:57 +0700

    update DetailActivity dengan memanggil class WarPlosDetailFragment dan update MainActivity dengan kondisional untuk phone atau tablet

--- app/src/main/java/com/example/glmvn/warplos/DetailActivity.java
+++ app/src/main/java/com/example/glmvn/warplos/DetailActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
+    public static final String EXTRA_WARPLOS_ID = "id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        WarplosDetailFragment warplosDetailFragment = (WarplosDetailFragment)
+                getFragmentManager().findFragmentById(R.id.detail_frag);
+        int warplosId = (int) getIntent().getExtras().get(EXTRA_WARPLOS_ID);
+        warplosDetailFragment.setWarplosID(warplosId);
     }
 }
--- app/src/main/java/com/example/glmvn/warplos/MainActivity.java
+++ app/src/main/java/com/example/glmvn/warplos/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.glmvn.warplos;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements WarplosListFragment.WarPlosListListener {
 
@@ -16,12 +18,19 @@ public class MainActivity extends AppCompatActivity implements WarplosListFragme
 
     @Override
     public void itemClicked(long id) {
-        WarplosDetailFragment detailFragment = new WarplosDetailFragment();
-        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-        detailFragment.setWarplosID(id);
-        fragmentTransaction.replace(R.id.fragment_container, detailFragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        fragmentTransaction.commit();
+        View fragmentContaner = findViewById(R.id.fragment_container);
+        if (fragmentContaner != null) {
+            WarplosDetailFragment detailFragment = new WarplosDetailFragment();
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            detailFragment.setWarplosID(id);
+            fragmentTransaction.replace(R.id.fragment_container, detailFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        }else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WARPLOS_ID, (int)id);
+            startActivity(intent);
+        }
     }
 }
